a7b3642a58ef3933bae500c1571afde67ecb4df0
Filip Růžička <f@filipruzicka.com>
2022-02-18 18:32:05 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Compare Prusa printers</title>
+      </Head>
+
+      <main>
+		<h1>Hey there!</h1>
+	  </main>
+    </div>
+  )
+}
