f2c32cf2abc2386d9ea702e577896aa883000abf
Filip Růžička <f@filipruzicka.com>
2022-02-20 10:41:43 +0100

    Load data using getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { getData } from "./api/list";
 
-export default function Home() {
+export default function CompareTable({ data }) {
   return (
     <div className="container">
       <Head>
@@ -8,8 +9,37 @@ export default function Home() {
       </Head>
 
       <main>
-		<h1>Hey there!</h1>
-	  </main>
+        <h1>Hey there!</h1>
+        <table>
+          <tbody>
+            <tr>
+              <th>Title</th>
+              <th>Build volume</th>
+              <th>Layer height</th>
+              <th>Max travel speed</th>
+              <th>Max temperatures</th>
+              <th>Controller</th>
+              <th>Filament diameter</th>
+            </tr>
+
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.title}</td>
+                <td>{item.buildVolume}</td>
+                <td>{item.layerHeight}</td>
+                <td>{item.maxTravelSpeed}</td>
+                <td>{item.maxTemperatures}</td>
+                <td>{item.controller}</td>
+                <td>{item.filamentDiameter}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  return { props: getData() };
 }
